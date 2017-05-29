4d8a10700a944191575972df96616a638d65daae
Piotr Kamiński <kanion92@gmail.com>
2017-05-29 22:06:46 +0200

    adding scss

--- webpack.config.js
+++ webpack.config.js
@@ -2,22 +2,23 @@ const path = require('path');
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
-    template: './src/index.html',
-    filename: 'index.html',
-    inject: 'body'
+	template: './src/index.html',
+	filename: 'index.html',
+	inject: 'body'
 });
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        path: path.resolve('dist'),
-        filename: 'index_bundle.js'
-    },
-    module: {
-        loaders: [
-            {test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/},
-            {test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/}
-        ]
-    },
-    plugins: [HtmlWebpackPluginConfig]
+	entry: './src/index.js',
+	output: {
+		path: path.resolve('dist'),
+		filename: 'index_bundle.js'
+	},
+	module: {
+		loaders: [
+			{test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/},
+			{test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/},
+			{test: /\.scss$/, loaders: ["style-loader", "css-loader", "sass-loader"], exclude: /node_modules/},
+		]
+	},
+	plugins: [HtmlWebpackPluginConfig]
 };
